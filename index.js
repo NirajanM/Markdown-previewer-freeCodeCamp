a450959c08073356fa2dd8b31ec42a24167f8fa4
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-17 07:55:51 +0545

    created state and imported marked library ready for use

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+marked.setOptions({
+    break: true
+})
+const renderer = new marked.Renderer;
 
 function App() {
-
+    const [text, setText] = React.useState("");
+    function handleChange(e) {
+        const value = e.target.value;
+        setText(value);
+    }
     return (
         <>
-            <textarea id="editor"></textarea>
-            <div id="preview"></div>
+            <h1>Markdown Previewer</h1>
+            <textarea id="editor" value={text} onChange={handleChange} placeholder="write markdown text here..."></textarea>
+            <h1>Output:</h1>
+            <div id="preview" dangerouslySet></div>
         </>
     )
 }
