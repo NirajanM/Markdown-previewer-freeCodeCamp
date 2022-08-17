4ce14f21cb0be692f05d7734b66abf39fd5b4867
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-17 08:40:58 +0545

    almost completed #4

--- index.js
+++ index.js
@@ -2,17 +2,65 @@ marked.setOptions({
     break: true
 })
 const renderer = new marked.Renderer;
+const defaultMarkdown = `
+Heading
+=======
+
+Sub-heading
+-----------
+ 
+### Another deeper heading
+ 
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a  
+line break
+
+Text attributes *italic*, **bold**, 
+\`monospace\`, ~~strikethrough~~.
+
+Shopping list:
+
+  * apples
+  * oranges
+  * pears
+
+Numbered list:
+
+  1. apples
+  2. oranges
+  3. pears
+
+The rain---not the reign---in
+Spain.
+
+\`\`\`
+Hey, look, a code block!
+\`\`\`
+
+ *[Helder S Ribeiro](https://freecodecamp.com/hsribei)*
+`;
 
 function App() {
-    const [text, setText] = React.useState("");
+    const [text, setText] = React.useState(defaultMarkdown);
     function handleChange(e) {
         const value = e.target.value;
         setText(value);
     }
+
     return (
         <>
             <h1>Markdown Previewer</h1>
-            <textarea id="editor" value={text} onChange={handleChange} placeholder="write markdown text here..."></textarea>
+            <textarea
+                id="editor"
+                value={text}
+                onChange={handleChange}
+                rows="10"
+                placeholder="write markdown text here..."
+            >
+
+            </textarea>
             <h1>Output:</h1>
             <Preview markdown={text} />
         </>
@@ -26,7 +74,8 @@ function Preview({ markdown }) {
             dangerouslySetInnerHTML={{
                 __html: marked.parse(markdown, { renderer: renderer }),
             }}
-        ></div>
+        >
+        </div>
     )
 }
 
