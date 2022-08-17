686796e4d46d08e81708b98d9f11a7c4a766b78e
Nirajan Malla <nirajannitro5@gmail.com>
2022-08-17 07:47:37 +0545

    starting files

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function App() {
+
+    return (
+        <>
+            <textarea id="editor"></textarea>
+            <div id="preview"></div>
+        </>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
